c5e0e7fd3d7b8741fb624672d1565d872d722647
qdenisq <qdenisq@gmail.com>
2018-09-20 16:06:50 +1000

    Revert "DS: best cl_vae so far"
    This reverts commit 1643c5581bd5e0aa234d7da6ac5f2b60ef43a3e6.

--- src/pytorch_cl_vae/utils.py
+++
@@ -1,7 +0,0 @@
-import seaborn
-seaborn.set()
-import matplotlib.pyplot as plt
-
-def plot_latent(z, labels):
-    seaborn.scatterplot(x=z[:, 0], y=z[:, 1], hue=labels,  legend="full", palette='Set2')
-
